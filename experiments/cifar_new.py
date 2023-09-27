68777fc981cd550ab298a38b82bb8d590acf2bfa
Ini <ioguntol@gmail.com>
2023-09-27 11:58:17 -0400

    Offload most of the training logic to train.py

---
+++ experiments/cifar_new.py
@@ -0,0 +1,74 @@
+import torch.nn as nn
+from torchvision.models.resnet import resnet18, ResNet18_Weights
+
+from data import get_data_loaders
+from models import ConceptBottleneckModel, ConceptWhiteningModel
+from train import train
+
+
+
+### Data
+
+train_loader, test_loader, CONCEPT_DIM = get_data_loaders('cifar100')
+OUTPUT_DIM = 100
+
+
+
+### Models
+
+def make_ffn(input_dim, output_dim, hidden_dim=256):
+    return nn.Sequential(
+        nn.Flatten(),
+        nn.Linear(input_dim, hidden_dim), nn.ReLU(),
+        nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+        nn.Linear(hidden_dim, output_dim),
+    )
+
+def make_resnet(output_dim):
+    resnet = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+    resnet.fc = nn.Linear(resnet.fc.in_features, output_dim)
+    return resnet
+
+def make_bottleneck_model(residual_dim):
+    return ConceptBottleneckModel(
+        concept_network=nn.Sequential(make_resnet(CONCEPT_DIM), nn.Sigmoid()),
+        residual_network=make_resnet(residual_dim),
+        target_network=make_ffn(CONCEPT_DIM + residual_dim, OUTPUT_DIM),
+    ).to('cuda')
+
+def make_whitening_model(residual_dim):
+    bottleneck_dim = CONCEPT_DIM + residual_dim
+    return ConceptWhiteningModel(
+        base_network=make_resnet(bottleneck_dim),
+        target_network=make_ffn(bottleneck_dim, OUTPUT_DIM),
+        bottleneck_dim=bottleneck_dim,
+    ).to('cuda')
+
+
+
+if __name__ == '__main__':
+    models = train(
+        make_bottleneck_model_fn=make_bottleneck_model,
+        make_whitening_model_fn=make_whitening_model,
+        concept_dim=CONCEPT_DIM,
+        residual_dim=32,
+        train_loader=train_loader,
+        test_loader=test_loader,
+        save_dir='./saved_models',
+        save_interval=10,
+        lr=1e-4,
+        num_epochs=100,
+        mi_estimator_hidden_dim=256,
+        mi_optimizer_lr=0.001,
+        whitening_alignment_frequency=20,
+    )
+
+    no_residual_model = models[0]
+    latent_residual_model = models[1]
+    decorrelated_residual_model = models[2]
+    mi_residual_model = models[3]
+    whitened_residual_model = models[4]
+
+    # TODO: experiments
+    # e.g. concept accuracy, interventions,
+    # train model to predict label from residual only, etc ...
---
+++ train.py
@@ -0,0 +1,225 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from datetime import datetime
+from pathlib import Path
+from torch.utils.data import DataLoader
+from typing import Callable
+
+from club import CLUB
+from models import ConceptBottleneckModel, ConceptWhiteningModel
+from utils import (
+    accuracy,
+    cross_correlation,
+    get_cw_callback_fn,
+    get_mi_callback_fn,
+    train_multiclass_classification,
+)
+
+
+
+def train_bottleneck_joint(
+    model: ConceptBottleneckModel,
+    train_loader: DataLoader,
+    test_loader: DataLoader | None = None,
+    residual_loss_fn: Callable = lambda r, c: torch.tensor(0),
+    alpha: float = 1.0,
+    beta: float = 1.0,
+    **kwargs) -> ConceptBottleneckModel:
+    """
+    Joint training of a concept bottleneck model.
+
+    Parameters
+    ----------
+    model : ConceptBottleneckModel
+        Model to train
+    train_loader : DataLoader
+        Train data loader
+    test_loader : DataLoader
+        Test data loader
+    residual_loss_fn : Callable(residual, concept_preds) -> Tensor
+        Function to compute the residual loss
+    alpha : float
+        Weight of the concept loss
+    beta : float
+        Weight of the residual loss
+    **kwargs
+        Additional arguments to pass to `train_multiclass_classification()`
+    """
+    def loss_fn(data, output, target):
+        _, concepts = data
+        concept_preds, residual, target_preds = output
+        concept_target = concepts[..., :concept_preds.shape[-1]]
+        concept_loss = nn.BCELoss()(concept_preds, concept_target)
+        residual_loss = residual_loss_fn(residual, concept_preds)
+        target_loss = nn.CrossEntropyLoss()(target_preds, target)
+        return (alpha * concept_loss) + (beta * residual_loss) + target_loss
+
+    model = train_multiclass_classification(
+        model,
+        train_loader,
+        preprocess_fn=lambda batch: (batch[0][0], batch[1]),
+        loss_fn=loss_fn,
+        **kwargs,
+    )
+
+    if test_loader is not None:
+        print(
+            'Test Classification Accuracy:',
+            accuracy(
+                model, test_loader,
+                preprocess_fn=lambda batch: (batch[0][0], batch[1]),
+            )
+        )
+
+def train_whitening(
+    model: ConceptWhiteningModel,
+    concept_dim: int,
+    train_loader: DataLoader,
+    test_loader: DataLoader | None = None,
+    alignment_frequency: int = 20,
+    **kwargs):
+    """
+    Train a concept whitening model.
+
+    Parameters
+    ----------
+    model : ConceptWhiteningModel
+        Model to train
+    concept_dim : int
+        Number of concepts
+    train_loader : DataLoader
+        Train data loader
+    test_loader : DataLoader
+        Test data loader
+    alignment_frequency : int
+        Frequency at which to align the concept whitening layer (i.e. every N batches)
+    **kwargs
+        Additional arguments to pass to `train_multiclass_classification()`
+    """
+    train_multiclass_classification(
+        model, train_loader,
+        preprocess_fn=lambda batch: (batch[0][0], batch[1]),
+        callback_fn=get_cw_callback_fn(
+            train_loader, concept_dim, alignment_frequency=alignment_frequency),
+        **kwargs,
+    )
+
+    if test_loader is not None:
+        print(
+            'Test Classification Accuracy:',
+            accuracy(
+                model, test_loader,
+                preprocess_fn=lambda batch: (batch[0][0], batch[1]),
+            )
+        )
+
+def train(
+    make_bottleneck_model_fn: Callable,
+    make_whitening_model_fn: Callable,
+    concept_dim: int,
+    residual_dim: int,
+    train_loader: DataLoader,
+    test_loader: DataLoader | None = None,
+    save_dir: str | Path = './saved_models',
+    mi_estimator_hidden_dim: int = 256,
+    mi_optimizer_lr: float = 0.001,
+    whitening_alignment_frequency : int = 20,
+    **kwargs) -> list[nn.Module]:
+    """
+    Trains the following models:
+        * Concept bottleneck model without residual
+        * Concept bottleneck model with latent residual
+        * Concept bottleneck model with decorrelated residual
+        * Concept bottleneck model with mutual information minimizing residual
+        * Concept whitening model with concept-whitened residual
+
+    Parameters
+    ----------
+    make_bottleneck_model_fn : Callable(residual_dim) -> ConceptBottleneckModel
+        Function to create a concept bottleneck model
+    make_whitening_model_fn : Callable(residual_dim) -> ConceptWhiteningModel
+        Function to create a concept whitening model
+    concept_dim : int
+        Number of concepts
+    residual_dim : int
+        Dimension of the residual
+    train_loader : DataLoader
+        Train data loader
+    test_loader : DataLoader
+        Test data loader
+    save_dir : str | Path
+        Directory to save the trained models
+    mi_estimator_hidden_dim : int
+        Hidden dimension of the mutual information estimator
+    mi_optimizer_lr : float
+        Learning rate of the mutual information optimizer
+    whitening_alignment_frequency : int
+        Frequency at which to align the concept whitening layer (i.e. every N batches)
+    **kwargs
+        Additional arguments to pass to `train_multiclass_classification()`
+    """
+    trained_models = []
+
+    # Create save directory
+    dataset_name = train_loader.dataset.__class__.__name__
+    date = datetime.today().strftime("%Y-%m-%d_%H_%M_%S")
+    save_dir = Path(save_dir) / dataset_name / date
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    # Train without residual
+    model = make_bottleneck_model_fn(residual_dim=0)
+    train_bottleneck_joint(
+        model, train_loader,
+        test_loader=test_loader,
+        save_path=save_dir / 'no_residual.pt',
+        **kwargs,
+    )
+    trained_models.append(model)
+
+    # Train with latent residual
+    model = make_bottleneck_model_fn(residual_dim=residual_dim)
+    train_bottleneck_joint(
+        model, train_loader,
+        test_loader=test_loader,
+        save_path=save_dir / 'latent_residual.pt',
+    )
+    trained_models.append(model)
+
+    # With decorrelated residual
+    model = make_bottleneck_model_fn(residual_dim=residual_dim)
+    train_bottleneck_joint(
+        model, train_loader,
+        test_loader=test_loader,
+        residual_loss_fn=lambda r, c: cross_correlation(r, c).square().mean(),
+        save_path=save_dir / 'decorrelated_residual.pt',
+    )
+    trained_models.append(model)
+
+    # With MI-minimized residual
+    model = make_bottleneck_model_fn(residual_dim=residual_dim)
+    mi_estimator = CLUB(residual_dim, concept_dim, mi_estimator_hidden_dim)
+    mi_optimizer = optim.Adam(mi_estimator.parameters(), lr=mi_optimizer_lr)
+    train_bottleneck_joint(
+        model, train_loader,
+        test_loader=test_loader,
+        residual_loss_fn=mi_estimator.forward,
+        callback_fn=get_mi_callback_fn(mi_estimator, mi_optimizer),
+        save_path=save_dir / 'mi_residual.pt',
+    )
+    trained_models.append(model)
+
+    # With concept-whitened residual
+    model = make_whitening_model_fn(residual_dim=residual_dim)
+    train_whitening(
+        model, concept_dim, train_loader,
+        test_loader=test_loader,
+        save_path=save_dir / 'whitened_residual.pt',
+        alignment_frequency=whitening_alignment_frequency,
+        **kwargs,
+    )
+    trained_models.append(model)
+
+    open(save_dir / '.done', 'a').close()
+    return trained_models
