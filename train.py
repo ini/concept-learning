8b8ce5ac73e39e61c61cffe169e563c79ca357b3
Ini <ioguntol@gmail.com>
2023-10-10 16:51:18 -0400

    Clean up train.py

--- train.py
+++ train.py
@@ -9,13 +9,12 @@ import torch.optim as optim
 from datetime import datetime
 from pathlib import Path
 from ray import air, tune
-from torch import Tensor
 from torch.utils.data import DataLoader
 from typing import Any, Callable
 
 from lib.club import CLUB
 from loader import get_data_loaders, DATASET_NAMES
-from models import ConceptModel, ConceptBottleneckModel, ConceptWhiteningModel
+from models import ConceptModel, ConceptWhiteningModel
 from utils import (
     accuracy,
     cross_correlation,
@@ -27,41 +26,115 @@ from utils import (
 
 
 
-def chain_fns(*fns: Callable) -> Callable:
+### Creating & Training Concept Models
+
+def make_concept_model(
+    experiment_module_name: str,
+    model_type: str,
+    concept_dim: int,
+    residual_dim: int,
+    mi_estimator_hidden_dim: int,
+    mi_optimizer_lr: float,
+    whitening_alignment_frequency: int,
+    whitening_alignment_loader: DataLoader,
+    device: str = 'cuda' if torch.cuda.is_available() else 'cpu',
+    **kwargs) -> tuple[ConceptModel, Callable, Callable]:
     """
-    Return a chained function that sequentially calls
-    the provided functions in the given order.
+    Create a concept model.
 
     Parameters
     ----------
-    *fns : Callable
-        Functions to chain together
-    """
-    def chained_fn(*args, **kwargs):
-        for fn in fns:
-            fn(*args, **kwargs)
+    experiment_module_name : str
+        Name of the experiment module (e.g. 'experiments.cifar')
+    model_type : str
+        Model type
+    concept_dim : int
+        Size of concept vector
+    residual_dim : int
+        Size of residual vector
+    mi_estimator_hidden_dim : int
+        Hidden dimension of mutual information estimator
+    mi_optimizer_lr : float
+        Learning rate of mutual information estimator optimizer
+    whitening_alignment_frequency : int
+        Frequency of concept alignment for whitening (in epochs)
+    whitening_alignment_loader: DataLoader,
+        Data loader to use for concept alignment for whitening
+    device : str
+        Device to load model on
 
-    return chained_fn
-
-def concept_model_predict_fn(outputs: Tensor | tuple[Tensor]) -> Tensor:
+    Returns
+    -------
+    model : ConceptModel
+        Concept model
+    callback_fn : Callable(model, epoch, batch_index, batch)
+        Callback function triggered before each batch
+    residual_loss_fn : Callable(residual, concept_preds) -> Tensor
+        Function to compute the residual loss
     """
-    Prediction function for concept models.
+    config = locals().copy()
+    config.update(kwargs)
 
-    Parameters
-    ----------
-    outputs : Tensor or tuple[Tensor]
-        Model outputs
-    """
-    outputs = outputs[-1] if isinstance(outputs, tuple) else outputs
-    return outputs.argmax(dim=-1)
+    # Load experiment module
+    experiment_module = importlib.import_module(experiment_module_name)
+    make_bottleneck_model_fn = experiment_module.make_bottleneck_model
+    make_whitening_model_fn = experiment_module.make_whitening_model
+
+    # Initialize callback function and residual loss function
+    callback_fn = lambda *args, **kwargs: None
+    residual_loss_fn = lambda r, c: torch.tensor(0)
+
+    # No residual
+    if model_type == 'no_residual':
+        model = make_bottleneck_model_fn(dict(config, residual_dim=0)).to(device)
+
+    # With latent residual
+    elif model_type == 'latent_residual':
+        model = make_bottleneck_model_fn(config).to(device)
+
+    # With decorrelated residual
+    elif model_type == 'decorrelated_residual':
+        model = make_bottleneck_model_fn(config).to(device)
+        residual_loss_fn = lambda r, c: cross_correlation(r, c).square().mean()
+
+    # With MI-minimized residual
+    elif model_type == 'mi_residual':
+        model = make_bottleneck_model_fn(config).to(device)
+        mi_estimator = CLUB(
+            residual_dim,
+            concept_dim,
+            mi_estimator_hidden_dim,
+        ).to(device)
+        mi_optimizer = optim.Adam(
+            mi_estimator.parameters(), lr=mi_optimizer_lr)
+        callback_fn = get_mi_callback_fn(mi_estimator, mi_optimizer)
+        residual_loss_fn = mi_estimator.forward
+
+    elif model_type == 'whitened_residual':
+        model = make_whitening_model_fn(config).to(device)
+        callback_fn=get_cw_callback_fn(
+            whitening_alignment_loader, concept_dim,
+            alignment_frequency=whitening_alignment_frequency,
+        )
+
+    else:
+        raise ValueError('Unknown model type:', model_type)
+
+    return model, callback_fn, residual_loss_fn
 
 def train_concept_model(
     model: ConceptModel,
     train_loader: DataLoader,
     val_loader: DataLoader,
-    config: dict[str, Any],
-    callback_fn: Callable = lambda model, epoch, batch_index, batch: None,
-    residual_loss_fn: Callable = lambda r, c: torch.tensor(0)):
+    num_epochs: int,
+    lr: float,
+    alpha: float,
+    beta: float,
+    callback_fn: Callable,
+    residual_loss_fn: Callable,
+    checkpoint_frequency: int,
+    verbose: bool = False,
+    **kwargs):
     """
     Train a concept model.
 
@@ -73,72 +146,137 @@ def train_concept_model(
         Train data loader
     val_loader : DataLoader
         Validation data loader
-    config : dict[str, Any]
-        Configuration dictionary
+    num_epochs : int
+        Number of epochs to train for
+    lr : float
+        Learning rate
+    alpha : float
+        Weight of concept loss
+    beta : float
+        Weight of residual loss
     callback_fn : Callable(model, epoch, batch_index, batch)
         Callback function triggered before each batch
     residual_loss_fn : Callable(residual, concept_preds) -> Tensor
         Function to compute the residual loss
+    checkpoint_frequency : int
+        How often to create model checkpoints (in epochs)
+    verbose : bool
+        Whether to print training progress
     """
-    # Get callback function
-    if isinstance(model, ConceptWhiteningModel):
-        cw_callback_fn = get_cw_callback_fn(
-            train_loader, config['concept_dim'],
-            alignment_frequency=config['whitening_alignment_frequency'],
-        )
-        callback_fn = chain_fns(callback_fn, cw_callback_fn)
-
     # Define loss function
-    if isinstance(model, ConceptBottleneckModel):
-        alpha, beta = config['alpha'], config['beta']
+    if isinstance(model, ConceptWhiteningModel):
+        loss_fn = lambda batch, outputs: nn.CrossEntropyLoss()(outputs[-1], batch[1])
+    else:
         def loss_fn(batch, outputs):
             (_, concept_targets), targets = batch
-            concept_logits, residual, target_logits = outputs
-            concept_targets = concept_targets[..., :concept_logits.shape[-1]]
-            concept_loss = nn.BCELoss()(concept_logits, concept_targets)
-            residual_loss = residual_loss_fn(residual, concept_logits)
+            concept_preds, residual, target_logits = outputs
+            concept_targets = concept_targets[..., :concept_preds.shape[-1]]
+            concept_loss = nn.BCELoss()(concept_preds, concept_targets)
+            residual_loss = residual_loss_fn(residual, concept_preds)
             target_loss = nn.CrossEntropyLoss()(target_logits, targets)
             return (alpha * concept_loss) + (beta * residual_loss) + target_loss
-    else:
-        loss_fn = lambda batch, outputs: nn.CrossEntropyLoss()(outputs, batch[1])
 
     # Train the model
     train_multiclass_classification(
         model,
         train_loader,
         val_loader,
-        num_epochs=config['num_epochs'],
-        lr=config['lr'],
+        num_epochs=num_epochs,
+        lr=lr,
         callback_fn=callback_fn,
         loss_fn=loss_fn,
-        predict_fn=concept_model_predict_fn,
+        predict_fn=lambda outputs: outputs[-1].argmax(dim=-1),
         save_path='./model.pt',
-        checkpoint_frequency=config['checkpoint_frequency'],
-        verbose=config.get('verbose', False),
+        checkpoint_frequency=checkpoint_frequency,
+        verbose=verbose,
     )
 
     # Test the model
     acc = accuracy(
         model, val_loader,
-        predict_fn=concept_model_predict_fn,
+        predict_fn=lambda outputs: outputs[-1].argmax(dim=-1),
     )
-    if config.get('verbose', False):
+    if verbose:
         print('Validation Classification Accuracy:', acc)
 
-def train(config: dict):
+
+
+### Ray Training Configuration
+
+def config_get(config: dict[str, Any], key: str):
+    """
+    Get a value from a Ray-style configuration dictionary
+    (handles top-level grid search).
+
+    Parameters
+    ----------
+    config : dict[str, Any]
+        Configuration dictionary
+    key : str
+        Configuration key
+    """
+    if 'grid_search' in config:
+        return config['grid_search'][0][key]
+    else:
+        return config[key]
+
+def config_set(config: dict[str, Any], key: str, value: Any):
+    """
+    Set a value in a Ray-style configuration dictionary
+    (handles top-level grid search).
+
+    Parameters
+    ----------
+    config : dict[str, Any]
+        Configuration dictionary
+    key : str
+        Configuration key
+    value : Any
+        Configuration value
+    """
+    if 'grid_search' in config:
+        for config in config['grid_search']:
+            config[key] = value
+    else:
+        config[key] = value
+
+def get_train_config(args: argparse.Namespace) -> dict[str, Any]:
+    """
+    Get Ray-style train configuration dictionary from command line arguments.
+
+    Parameters
+    ----------
+    args : argparse.Namespace
+        Command line arguments
+    """
+    # Load provided experiment config
+    experiment_module = importlib.import_module(args.config)
+    config = experiment_module.get_config()
+    config_set(config, 'experiment_module_name', args.config)
+
+    # Override config with command line arguments
+    for key, value in vars(args).items():
+        if isinstance(value, list):
+            config_set(config, key, tune.grid_search(value))
+        elif value is not None:
+            config_set(config, key, value)
+
+    # Use absolute paths
+    config_set(config, 'data_dir', Path(config_get(config, 'data_dir')).resolve())
+    config_set(config, 'save_dir', Path(config_get(config, 'save_dir')).resolve())
+
+    return config
+
+def train(config: dict[str, Any]):
     """
     Create and train a model with the given configuration.
 
     Parameters
     ----------
-    config : dict
+    config : dict[str, Any]
         Configuration dictionary
+        (see arguments to `make_concept_model()` and `train_concept_model()`)
     """
-    experiment_module = importlib.import_module(config['experiment_module_name'])
-    make_bottleneck_model_fn = experiment_module.make_bottleneck_model
-    make_whitening_model_fn = experiment_module.make_whitening_model
-    device = config.get('device', 'cuda' if torch.cuda.is_available() else 'cpu')
-
     # Get data loaders
     train_loader, val_loader, _, concept_dim, num_classes = get_data_loaders(
         config['dataset'], data_dir=config['data_dir'], batch_size=config['batch_size'])
@@ -147,47 +285,17 @@ def train(config: dict):
     config['concept_dim'] = concept_dim
     config['num_classes'] = num_classes
 
-    # No residual
-    if config['model_type'] == 'no_residual':
-        model = make_bottleneck_model_fn(dict(config, residual_dim=0)).to(device)
-        train_concept_model(model, train_loader, val_loader, config)
+    # Create model, callback function, and residual loss function
+    model, callback_fn, residual_loss_fn = make_concept_model(
+        whitening_alignment_loader=train_loader, **config)
 
-    # With latent residual
-    elif config['model_type'] == 'latent_residual':
-        model = make_bottleneck_model_fn(config).to(device)
-        train_concept_model(model, train_loader, val_loader, config)
-
-    # With decorrelated residual
-    elif config['model_type'] == 'decorrelated_residual':
-        model = make_bottleneck_model_fn(config).to(device)
-        train_concept_model(
-            model, train_loader, val_loader, config,
-            residual_loss_fn=lambda r, c: cross_correlation(r, c).square().mean(),
-        )
-
-    # With MI-minimized residual
-    elif config['model_type'] == 'mi_residual':
-        model = make_bottleneck_model_fn(config).to(device)
-        mi_estimator = CLUB(
-            config['residual_dim'],
-            config['concept_dim'],
-            config['mi_estimator_hidden_dim']
-        ).to(device)
-        mi_optimizer = optim.Adam(
-            mi_estimator.parameters(), lr=config['mi_optimizer_lr'])
-        train_concept_model(
-            model, train_loader, val_loader, config,
-            residual_loss_fn=mi_estimator.forward,
-            callback_fn=get_mi_callback_fn(mi_estimator, mi_optimizer),
-        )
-
-    # With concept-whitened residual
-    elif config['model_type'] == 'whitened_residual':
-        model = make_whitening_model_fn(config).to(device)
-        train_concept_model(model, train_loader, val_loader, config)
-
-    else:
-        raise ValueError('Unknown model type:', config['model_type'])
+    # Train the model
+    train_concept_model(
+        model, train_loader, val_loader,
+        callback_fn=callback_fn,
+        residual_loss_fn=residual_loss_fn,
+        **config,
+    )
 
 
 
@@ -246,43 +354,27 @@ if __name__ == '__main__':
         '--checkpoint-frequency', type=int, nargs='+', help='Frequency of checkpointing')
 
     args = parser.parse_args()
-
-    # Load provided experiment config
-    experiment_module = importlib.import_module(args.config)
-    experiment_config = experiment_module.get_config()
-    experiment_config['experiment_module_name'] = args.config
-
-    # Override experiment config with command line arguments
-    for key, value in vars(args).items():
-        if isinstance(value, list):
-            experiment_config[key] = tune.grid_search(value)
-        elif value is not None:
-            experiment_config[key] = value
-
-    # Use absolute paths
-    experiment_config['data_dir'] = Path(experiment_config['data_dir']).resolve()
-    experiment_config['save_dir'] = Path(experiment_config['save_dir']).resolve()
+    config = get_train_config(args)
 
     # Get experiment name
     date = datetime.today().strftime("%Y-%m-%d_%H_%M_%S")
-    experiment_name = experiment_module.__name__.split('.')[-1]
+    experiment_name = config_get(config, 'experiment_module_name').split('.')[-1]
     experiment_name = f'{experiment_name}/{date}/train'
 
+    # Get number of GPUs
+    try:
+        num_gpus = config_get(config, 'num_gpus')
+    except:
+        num_gpus = 1 if torch.cuda.is_available() else 0
+
     # Train the model(s)
-    num_gpus = experiment_config.get('num_gpus', 1)
     tuner = tune.Tuner(
-        tune.with_resources(
-            train,
-            resources={
-                'cpu': 1,
-                'gpu': num_gpus if torch.cuda.is_available() else 0
-            },
-        ),
-        param_space=experiment_config,
+        tune.with_resources(train, resources={'cpu': 1, 'gpu': num_gpus}),
+        param_space=config,
         tune_config=tune.TuneConfig(metric='val_acc', mode='max', num_samples=1),
         run_config=air.RunConfig(
             name=experiment_name,
-            storage_path=experiment_config['save_dir'],
+            storage_path=config['save_dir'],
             checkpoint_config=air.CheckpointConfig(
                 checkpoint_score_attribute='val_acc',
                 checkpoint_score_order='max',
