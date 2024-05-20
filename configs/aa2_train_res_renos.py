84f75907f5efb1060c20bcee7b762efbafbea38d
Renos Zabounidis <rzabounidis@protonmail.com>
2024-05-19 21:55:54 -0700

    aa2 config

--- configs/aa2_train_res_renos.py
+++ configs/aa2_train_res_renos.py
@@ -7,13 +7,19 @@ from experiments.aa2 import (
 
 def get_config(**kwargs) -> dict:
     experiment_config = {
-        "model_type": "latent_residual",
+        "model_type": tune.grid_search(
+            [
+                "latent_residual",
+                "decorrelated_residual",
+                "mi_residual",
+            ]
+        ),
         "save_dir": "/data/renos/supervised_concept_learning/",
         "data_dir": "/data/Datasets/",
         "ray_storage_dir": "/data/renos/ray_results/",
-        "residual_dim": 2,
+        "residual_dim": tune.grid_search([1, 2, 4, 8, 16, 32, 64]),
         "lr": 1e-4,
-        "num_epochs": 100,
+        "num_epochs": 200,
         "alpha": 1.0,
         "beta": 1.0,
         "mi_estimator_hidden_dim": 256,
@@ -22,7 +28,7 @@ def get_config(**kwargs) -> dict:
         "num_cpus": 8,
         "num_gpus": 1.0,
         "num_samples": 1,
-        "batch_size": 8,
+        "batch_size": 64,
         "checkpoint_frequency": 5,
         "norm_type": "layer_norm",
         "T_whitening": 3,
